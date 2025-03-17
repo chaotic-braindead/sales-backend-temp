65afe184420b4c7e766d599ebe079e7072e07d5a
Rafael Torres <rafaelsebastian0908@gmail.com>
2025-03-17 16:31:46 +0800

    update database settings

--- sales_backend/sales_backend/settings.py
+++ sales_backend/sales_backend/settings.py
@@ -94,13 +94,23 @@ WSGI_APPLICATION = "sales_backend.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "kinetiq",
+        "USER": "root",
+        "PASSWORD": "",
+        "HOST": "localhost",
+        "PORT": 3306,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators

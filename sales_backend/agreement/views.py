d5f1ea05ce0eb91cce171252583e76b5b58f88fb
Rafael Torres <rafaelsebastian0908@gmail.com>
2025-03-18 14:38:41 +0800

    add agreement creation

--- sales_backend/agreement/views.py
+++ sales_backend/agreement/views.py
@@ -1,7 +1,44 @@
 from rest_framework import viewsets
 from .serializers import *
+from django.shortcuts import get_list_or_404, get_object_or_404
+from rest_framework.response import Response
+from rest_framework.request import Request
+from quotation.models import Quotation, QuotationItems
+from rest_framework import status
 
 
 class BlanketAgreementViewSet(viewsets.ModelViewSet):
     queryset = BlanketAgreement.objects.all()
     serializer_class = BlanketAgreementSerializer
+
+    def create(self, request: Request, *args, **kwargs):
+        # convert quotation_id to sales order
+        quotation_id = request.data.pop("quotation_id")
+        quotation = get_object_or_404(Quotation, pk=quotation_id)
+
+        body = request.data
+
+        quotation_items: list[QuotationItems] = get_list_or_404(
+            QuotationItems, quotation_id=quotation
+        )
+        to_create = []
+
+        agreement = BlanketAgreement.objects.create(
+            customer_id=quotation.customer_id,
+            salesrep_id=quotation.salesrep_id,
+            total_value=quotation.total_amount,
+            **body
+        )
+
+        for item in quotation_items:
+            data = {
+                "agreement_id": agreement,
+                "product_id": item.product_id,
+                "agreed_quantity": item.quantity,
+                "unit_price": item.unit_price,
+            }
+            to_create.append(BlanketAgreementItems(**data))
+        BlanketAgreementItems.objects.bulk_create(to_create)
+        return Response(
+            BlanketAgreementSerializer(agreement).data, status=status.HTTP_201_CREATED
+        )
--- sales_backend/order/README.md
+++ sales_backend/order/README.md
@@ -6,6 +6,7 @@ Sample request body:
 {
     "quotation_id": 98,
     "order_type": "Direct",
+    "order_date": "2025-03-16",
     "markup": 10
 }
 ```

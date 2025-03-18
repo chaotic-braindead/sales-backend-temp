f684a0c73094a84779cee14ce623d2498d39dd3a
Rafael Torres <rafaelsebastian0908@gmail.com>
2025-03-18 20:59:52 +0800

    update agreement

--- sales_backend/agreement/serializers.py
+++ sales_backend/agreement/serializers.py
@@ -4,16 +4,9 @@ from misc.serializers import ProductSerializer
 
 
 class BlanketAgreementItemsSerializer(serializers.ModelSerializer):
-    product_id = ProductSerializer()
-
     class Meta:
         model = BlanketAgreementItems
         fields = "__all__"
-
-    def to_representation(self, instance):
-        data = super().to_representation(instance)
-        data["product"] = data.pop("product_id")
-        return data
 
 
 class BlanketAgreementSerializer(serializers.ModelSerializer):

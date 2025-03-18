3817666189103fe81e730553e003dc7b5f08a974
Rafael Torres <rafaelsebastian0908@gmail.com>
2025-03-18 21:00:15 +0800

    update costing

--- sales_backend/costing/serializers.py
+++ sales_backend/costing/serializers.py
@@ -1,18 +1,8 @@
 from rest_framework import serializers
 from .models import *
-from misc.serializers import NonProjectOrderPricingSerializer, BOMSerializer
 
 
 class SalesCostingSerializer(serializers.ModelSerializer):
-    non_project_costing_id = NonProjectOrderPricingSerializer()
-    bom_id = BOMSerializer()
-
     class Meta:
         model = SalesCosting
         fields = "__all__"
-
-    def to_representation(self, instance):
-        data = super().to_representation(instance)
-        data["non_project_costing"] = data.pop("non_project_costing_id")
-        data["bom"] = data.pop("bom_id")
-        return data
